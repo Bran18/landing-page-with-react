888fc1dbeb68d7d30cf7b0e007d48a2048e629de
Brandon Fernández <andreyfdez18@gmail.com>
2021-02-25 03:01:09 +0000

    adding Navbar and Jumbotron

---
+++ src/js/component/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const NavBar = () => {
+	return (
+		<div>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<a className="navbar-brand" href="#">
+					Landing Page React
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarText"
+					aria-controls="navbarText"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarText">
+					<ul className="navbar-nav ml-auto">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Features
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Pricing
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	);
+};
+
+export default NavBar;
